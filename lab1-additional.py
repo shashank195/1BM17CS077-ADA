815f2b3e8dc189d8570c29a61ba50a9cbeaaa662
Rohan <rohanmayya.cs17@bmsce.ac.in>
2019-08-07 13:07:29 +0530

    testcase file created

--- lab1-additional.py
+++ lab1-additional.py
@@ -1,8 +1,8 @@
 def binary_search(num_array, n, key) -> int:
     low = 0
     high = n
-    while low<=high:
-        mid=int((low+high)/2)
+    while low <= high:
+        mid =int((low+high)/2)
         if key == num_array[mid]:
             return 1
         elif key < num_array[mid]:
@@ -11,5 +11,16 @@ def binary_search(num_array, n, key) -> int:
             low = mid + 1
     return -1
 
-result = binary_search([1,5,7], 3, 0)
-print("Result:", result)
+
+def read_test_case_file():
+    input = open("testcase.txt")
+    n = input.readlines()
+    for lines in n:
+        a = map(int, lines.split())
+
+read_test_case_file()
+# result = binary_search([1,5,7], 3, 0)
+# print("Result:", result)
+
+# for i in line1_testcases:
+#     binarysearch(line3,line2_n,line2_key)
