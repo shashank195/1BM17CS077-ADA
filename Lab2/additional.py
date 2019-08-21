b20f83a92689766037e72e3ef4022cd34d00422f
Rohan <rohanmayya.cs17@bmsce.ac.in>
2019-08-21 12:36:15 +0530

    wrote algorithm to find pivot in O(log n) time

--- Lab2/additional.py
+++ Lab2/additional.py
@@ -11,11 +11,30 @@ def binary_search(num_array, key) -> int:
             low = mid + 1
     return -1
 
+def findPivot(arr, low, high):
+    if high < low: 
+        return -1
+    if high == low:
+        return low
+    mid = int((low+high)/2)
+
+    if mid < high and arr[mid] > arr[mid+1]:
+        return mid
+    
+    if mid > low and arr[mid] < arr[mid-1]:
+        return mid-1
+    
+    if arr[low] >= arr[mid]:
+        return findPivot(arr, low, mid-1)
+    return findPivot(arr, mid+1, high)
+
+
 def search_element(arr, key):
-    pivot = arr.index(max(arr))
+    # pivot = arr.index(max(arr))
+    pivot = findPivot(arr, 0, len(arr)-1)
     if key >= arr[0]:
         return binary_search(arr[:pivot+1], key)
     else:
         return binary_search(arr[pivot+1:], key)
 
-print(search_element([3,4,5,1,2], 6))
+print(search_element([3,4,5,1,2], 2))
