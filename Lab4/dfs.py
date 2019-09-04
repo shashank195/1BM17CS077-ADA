808b2db7ea2a2d792c7c66afe36f1616de04c035
Rohan <rohanmayya.cs17@bmsce.ac.in>
2019-09-04 11:48:23 +0530

    finding number of connected components

--- Lab4/dfs.py
+++ Lab4/dfs.py
@@ -1,5 +1,5 @@
 def dfs(v):
-    print(v)
+    print(v, end="")
     visited[v] = 1
     for i in range(4): # to number of nodes
         if adjacency_matrix[v][i] == 1 and visited[i] == 0:
@@ -13,4 +13,8 @@ adjacency_matrix = [[0,1,0,1], [1,0,1,0], [0,1,0,1], [1,0,1,0]]
 for i in range(n):
     visited.append(0)
 
-dfs(1)s
+for i in range(n):
+    if visited[i] == 0:
+        dfs(i)
+
+print()
