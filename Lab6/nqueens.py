fc5ae003d77b5303553027ef04071def3fba40b8
Rohan <rohanmayya.cs17@bmsce.ac.in>
2019-09-25 12:46:27 +0530

    nqueens complete

--- Lab6/nqueens.py
+++ Lab6/nqueens.py
@@ -3,19 +3,21 @@
 # k rows, i columns
 
 n, k = 4, 0
-x = [-1]*(n)
+x = [-1]*n
 
 def NQueens(n,k):
+    if k == n:
+        print(x[0:n])
+        return
     for i in range(n):
         if place(k,i):
-            x[k] = i
-            if k == n:
-                print(x[1:n])
-            else:
-                NQueens(n,k+1)
+            x[k] = i            
+            NQueens(n,k+1)
+            x[k] = -1
+    return
 
 def place(k, i):
-    for j in range(1, k):
+    for j in range(k):
         if x[j] == i or abs(x[j] - i) == abs(j-k):
             return False
     return True
